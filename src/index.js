9a19fe8c83ca9ee37d6b97e5379aed6e680195b6
root <gerard.ramosm89@gmail.com>
2017-01-09 14:44:09 -0800

    Queries youtube API successfully

--- src/index.js
+++ src/index.js
@@ -2,22 +2,25 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+
 const API_KEY = 'AIzaSyCW3cWPx6LtPMI7VQS9Wnd_OPaNWiQ18Rk';
 
-YTSearch({ key: API_KEY, term: 'surfboards'}, data => {
-	console.log(data);	
-});
 
 //Create a new Component. This component should produce some HTML
 class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = { videos: [] };
+		YTSearch({ key: API_KEY, term: 'pokemon'}, videos => {
+  		this.setState({ videos });	
+		});
 	}
 	render() {
 		return( 
 			<div>
 				<SearchBar />
+				<VideoList videos={this.state.videos} />
 			</div>
 		);
 	}
