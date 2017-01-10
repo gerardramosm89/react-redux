ebe54755fe3d483342a2eb6fdef17a30d4e27619
gramos <gerard.ramosm89@gmail.com>
2017-01-09 21:22:42 -0800

    slight changes

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,13 +8,16 @@ class SearchBar extends Component {
 	render() {
 		return (
 			<div className="search-bar">
-			<input 
+			<div className="jumbotron">
+				<h1>React Testing Grounds</h1>
+			</div>
+			<input placeholder="Enter a video you'd like to search for" 
 				value={this.state.term}
 			onChange={event => 
 				{	
 					this.onInputChange(event.target.value);
 				}
-			}/> 
+			}/><button className="btn btn-info">Search</button> 
 			</div>
 		);
 	}
